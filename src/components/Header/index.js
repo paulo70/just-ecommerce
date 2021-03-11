cf0ae377698016af4f69ce9235ed0bc30ebecabd
paulo70 <paulofrontend@gmail.com>
2021-03-10 21:09:06 -0300

    Add folder components and add header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBasket, faCashRegister, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Header = () => {
+  <header className='header'>
+    <h1 className='header-title'>
+      Books
+     <span className='header-title-sub'>store</span>
+    </h1>
+  </header> 
+}
+
+export default Header
