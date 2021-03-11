a72782a09597e8bb70d1741adc0ef68a7003f45f
paulo70 <paulofrontend@gmail.com>
2021-03-10 21:58:13 -0300

    Add style header and badge car

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,13 +4,61 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingBasket, faCashRegister, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
-const Header = () => {
-  <header className='header'>
-    <h1 className='header-title'>
-      Books
-     <span className='header-title-sub'>store</span>
-    </h1>
-  </header> 
+
+import './header.scss'
+
+const Header = props => {
+  return (
+    <>
+      <header className='header'>
+        <h1 className='header-title'>
+          Books
+         <span className='header-title-sub'>store</span>
+        </h1>
+      </header> 
+      <Navbar>
+        <Navbar.Collapse className='justify-content-end'>
+          <Nav>
+            <NavDropdown title = {
+                <div style = {{display:'inline-block'}}>
+                  <FontAwesomeIcon icon = {faShoppingCart} />
+                  &nbsp;
+                  Carrinho
+                </div>
+              }
+              drop='left'>
+
+              <NavDropdown.Item href='' onClick={props.handleShowItens}>
+                <FontAwesomeIcon icon={faShoppingBasket} />
+                  &nbsp;
+                <strong>Produtos</strong>
+              </NavDropdown.Item>
+
+              <NavDropdown.Divider />
+              
+
+              <NavDropdown.Divider />
+
+              <NavDropdown.Item href=''>
+                Total: R$ '0,00'
+              </NavDropdown.Item>
+
+              <span className=''>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='' onClick={() => props.handleCloseShopping(calculateTotal())}>
+                  <FontAwesomeIcon icon ={faCashRegister} />
+                  &nbsp;
+                  Finalizar compra
+                </NavDropdown.Item>
+              </span>
+
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar> 
+      <div className='nav-bars'></div>
+    </>
+  )
 }
 
 export default Header
