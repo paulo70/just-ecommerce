d7868e8c6016c598a6d4a6c70e7fdc22176505a8
paulo70 <paulofrontend@gmail.com>
2021-03-11 22:07:08 -0300

    Add card to products list

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './card.scss'
+
+const Card = props => {
+  return(
+    <div className='card'>
+      { props.children }
+    </div>
+  )
+}
+
+export default Card
